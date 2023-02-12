9f0c546b97fd3f8b176fa0f01925dd72f7d82531
jyotiprakash055 <jyotiprakash.panigrahi055@gmail.com>
2023-02-12 19:11:35 +0530

    init

---
+++ src/components/formComponents/FormInput.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Input } from "@chakra-ui/react";
+import FormWrapper from "./FormWrapper";
+import { IFormInputProps } from "@src/interface/forms";
+import { useData } from "../../containers/home/DataProvider";
+
+
+const FormInput = React.forwardRef<HTMLInputElement, IFormInputProps>(
+  (
+    {
+      name,
+      label,
+      placeholder,
+      type,
+      value,
+      onChange,
+      onBlur,
+      error,
+      touched,
+      inputProps = {},
+      children,
+      helperText,
+      wrapperProps = {},
+    },
+    ref
+  ) => {
+
+    const contextValue = useData()
+
+    // console.log(contextValue?.state)
+
+    const handelChange = (e:any) => {
+      onChange?.(e)
+      // console.log(e.target.name)
+
+      // console.log(contextValue)
+      contextValue?.setState((prevValue: any) => {
+        return {
+          ...prevValue,
+          [e.target.name]: e.target.value
+        }
+      })
+
+
+
+    }
+    return (
+      <FormWrapper
+        isInvalid={Boolean(error && touched)}
+        wrapperProps={wrapperProps}
+        helperText={helperText}
+        label={label}
+        touched={touched}
+        error={error as string}
+      >
+        <Input
+          name={name}
+          placeholder={placeholder}
+          type={type}
+          value={value}
+          onChange={handelChange}
+          onBlur={onBlur}
+          // styles
+          width="100%"
+          maxHeight="none !important"
+          minW="272px"
+          height="45px"
+          fontSize="0.875rem"
+          fontWeight="500"
+          px="20px"
+          border="1px solid #c0bcd7"
+          bg="inputBg"
+          borderRadius="10px"
+          focusBorderColor="primary"
+          errorBorderColor="errorRed"
+          _placeholder={{
+            color: "text.placeholder",
+          }}
+          ref={ref}
+          {...inputProps}
+        />
+        {children}
+      </FormWrapper>
+    );
+  }
+);
+
+FormInput.displayName = "FormInput";
+
+export default FormInput;
---
+++ src/containers/home/DataProvider.tsx
@@ -0,0 +1,66 @@
+import React, { createContext, useContext, useState } from "react";
+import { useFormikContext } from 'formik';
+// const initialValues = {
+//   requisitionDetails: {
+//     gender: "",
+//     noOfOpenings: 0,
+//     requisitionTitle: "",
+//     urgency: "",
+//   },
+//   jobDetails: {
+//     jobDetails: "",
+//     jobLocation: "",
+//     jobTitle: "",
+//   },
+//   interviewSettings: {
+//     interviewDuration: "",
+//     interviewLanguage: "",
+//     interviewMode: "",
+//   },
+// };
+
+
+
+const initialValues = {
+ 
+    gender: "",
+    noOfOpenings: 0,
+    requisitionTitle: "",
+    urgency: "",
+    jobDetails: "",
+    jobLocation: "",
+    jobTitle: "",
+    interviewDuration: "",
+    interviewLanguage: "",
+    interviewMode: "",
+};
+
+export const DataContext = createContext<{
+  state: typeof initialValues;
+  setState: React.Dispatch<React.SetStateAction<any>>;
+} | null>(null);
+
+
+//typeof initialValues
+const DataProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [state, setState] = useState(initialValues);
+
+  return (
+    <DataContext.Provider value={{ state, setState }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+
+
+
+
+export const useData = () => {
+  const data = useContext(DataContext);
+  return data;
+};
+
+export default DataProvider
